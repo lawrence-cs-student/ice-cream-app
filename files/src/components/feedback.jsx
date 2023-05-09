0ad359d86672644c68667ee1afdb20d60639018c
lawrence-cs-student <114126573+lawrence-cs-student@users.noreply.github.com>
2023-05-09 12:17:44 +0800

    Update feedback.jsx

--- files/src/components/feedback.jsx
+++ files/src/components/feedback.jsx
@@ -19,7 +19,7 @@ export default function Feedback(props) {
             </div>
             <div className="second-child">
                 <div className="output">
-                    <img src="https://scontent.fmnl30-2.fna.fbcdn.net/v/t39.30808-6/312624354_1561560460960412_6438238438115084863_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=174925&_nc_eui2=AeENXAuP1HTq2irx-cv4Kq0k5QAH5EK0OsflAAfkQrQ6x_kg61BOpMfm99RWbDjY6W06GdwDyTH8MnomFVFZeMSa&_nc_ohc=MWeg83WWan0AX-t84VA&_nc_ht=scontent.fmnl30-2.fna&oh=00_AfCKEn5vu6pKUh8sjWOrWlhpDxqoMNKocRIX8PcSTkqk1w&oe=6455DFD1"></img>
+                    <img src="public/lyka.jpg"></img>
                     <h1>{props.data}</h1>
                     <img src="ice-cream.png"></img>
                 </div>
