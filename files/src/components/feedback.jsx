6fde1efbb51fa17416ae31ed1edacb71f4a644e1
lawrence-cs-student <114126573+lawrence-cs-student@users.noreply.github.com>
2023-08-09 10:50:13 +0800

    Update feedback.jsx

--- files/src/components/feedback.jsx
+++ files/src/components/feedback.jsx
@@ -7,10 +7,8 @@ export default function Feedback(props) {
         <div className="feedback-container">
             <div className="first-child">
                 <form onSubmit={props.clicked}>
-                    <div>
-                        <input placeholder="First Name"></input>
-                        <input placeholder="Last Name"></input>
-                    </div>
+                    <input placeholder="First Name"></input>
+                    <input placeholder="Last Name"></input>
                     <input placeholder="Email"></input>
                     <input placeholder="Item name"></input>
                     <input onChange={props.change} value={props.value} placeholder="Feedback"></input>
@@ -19,11 +17,12 @@ export default function Feedback(props) {
             </div>
             <div className="second-child">
                 <div className="output">
-                    <img src="lyka.jpg"></img>
+                    <img src="https://i.redd.it/fwlb9v0kv7f31.jpg"></img>
                     <h1>{props.data}</h1>
                     <img src="ice-cream.png"></img>
                 </div>
             </div>
+            <h1>SUNDAE SERENITY</h1>
         </div>
     )
 }
